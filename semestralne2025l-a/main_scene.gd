2fa1b516fc1e05e83d217d712dd27c11af3815eb
NamespaceV <s-zephyr@o2.pl>
2025-03-20 05:25:25 +0100

    initial commit,  sample project godot 4.4

---
+++ semestralne2025l-a/main_scene.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var button_teleport_area : Rect2
+@onready var score_label: Label = $ColorRect/Score
+@onready var click_me_button = $ClickMeButton
+var counter = 0
+
+func _ready() -> void:
+	click_me_button.pressed.connect(on_button_clicked)
+	score_label.text = ""
+
+func on_button_clicked() -> void:
+	var x = button_teleport_area.position.x + randf() * button_teleport_area.size.x
+	var y = button_teleport_area.position.y + randf() * button_teleport_area.size.y
+	click_me_button.position = Vector2(x, y)
+	counter += 1
+	score_label.text = "Score: %d" % [counter]
