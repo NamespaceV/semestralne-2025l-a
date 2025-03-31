4604ac00c374dc1ff0ecf1cc5e109e6846095384
NamespaceV <s-zephyr@o2.pl>
2025-03-31 06:57:24 +0200

    movement 3 example

---
+++ semestralne2025l-a/test/zephyr/character3.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+var speed := 600.0
+var rotation_speed := 3 * PI / 2 # per second
+var attack_distance := -100.0
+
+
+var attack_scene = load("res://test/zephyr/attack.tscn")
+
+func _physics_process(delta: float) -> void:
+
+	if Input.is_action_just_pressed("action"):
+		var attack = attack_scene.instantiate()
+		attack.name = "Attack"
+		attack.position = global_position + transform.x * 200
+		get_tree().current_scene.add_child(attack, true)
+
+	var target = get_global_mouse_position()
+	var angle = get_angle_to(target)
+	var rotation_angle = delta * rotation_speed
+	if abs(angle) <= rotation_angle:
+		rotate(angle)
+	else:
+		rotate(rotation_angle * sign(angle))
+	var new_velocity = 0
+	if Input.is_action_pressed("up"):
+		new_velocity += 1 * speed
+		velocity = new_velocity * transform.x
+		move_and_slide()
+	if Input.is_action_pressed("down"):
+		new_velocity -= 1 * speed
+		velocity = new_velocity * transform.x
+		move_and_slide()
