d6ac77af8c72e24c0a8bf261d31237414176021d
NamespaceV <s-zephyr@o2.pl>
2025-04-12 23:38:35 +0200

    loop zephyr test levels

--- semestralne2025l-a/test/zephyr/character3.gd
+++ semestralne2025l-a/test/zephyr/character3.gd
@@ -31,3 +31,7 @@ func _physics_process(delta: float) -> void:
 		new_velocity -= 1 * speed
 		velocity = new_velocity * transform.x
 		move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://test/zephyr/movement.tscn")
