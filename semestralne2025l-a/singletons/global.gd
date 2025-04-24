d3f93862e28dcb8033a22d5f4a454587544a44b5
NamespaceV <s-zephyr@o2.pl>
2025-04-25 01:54:55 +0200

    add toggle fullscreen key binding

--- semestralne2025l-a/singletons/global.gd
+++ semestralne2025l-a/singletons/global.gd
@@ -9,3 +9,10 @@ func _enter_tree() -> void:
 
 func go_to_main_menu():
 	get_tree().change_scene_to_file("res://main_scene.tscn")
+
+func _input(_event):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
