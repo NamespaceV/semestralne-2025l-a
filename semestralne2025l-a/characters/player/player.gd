da429826a3bb6ab8f9b8e6b695cdf675f2feb25e
michaldylo <m.dylo@onyxtms.com>
2025-05-20 23:19:48 +0200

    Add basic shooting

--- semestralne2025l-a/characters/player/player.gd
+++ semestralne2025l-a/characters/player/player.gd
@@ -2,12 +2,27 @@ extends CharacterBody2D
 class_name PlayerPICON
 
 const speed = 100.0
+var bullet_speed = 375
+var bullet = preload("res://scenes/bullet.tscn")
+
 
 func  get_input():
 	var input_direction = Input.get_vector("left","right","up", "down")
 	velocity = input_direction * speed
 
+
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	get_input()
 	move_and_slide()
+
+	if Input.is_action_just_pressed("action"):
+		fire()
+
+
+func fire():
+	var bullet_instance = bullet.instantiate()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.linear_velocity = Vector2(bullet_speed, 0).rotated(rotation)
+	get_tree().get_root().call_deferred("add_child", bullet_instance)
